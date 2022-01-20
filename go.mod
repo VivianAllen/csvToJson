4eaee9bd40357807a21969978e2a922cbb7be942
Vivian Allen <vivian.allen@methods.co.uk>
2022-01-20 15:35:09 +0000

    basic working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/v2/csvToJson
 
 go 1.16
+
+require github.com/gosimple/slug v1.12.0
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/gosimple/slug"
 )
 
 // Metadata defines model for Metadata.
@@ -12,6 +16,7 @@ type Metadata struct {
 	Code   *string `json:"code,omitempty"`
 	Name   *string `json:"name,omitempty"`
 	Slug   *string `json:"slug,omitempty"`
+	Desc   *string `json:"desc,omitempty"`
 	Tables *Tables `json:"tables,omitempty"`
 }
 
@@ -23,21 +28,22 @@ type Table struct {
 	Code       *string     `json:"code,omitempty"`
 	Name       *string     `json:"name,omitempty"`
 	Slug       *string     `json:"slug,omitempty"`
+	Desc       *string     `json:"desc,omitempty"`
+	Total      *Triplet    `json:"total,omitempty"`
 }
 
 // Tables defines model for Tables.
 type Tables []Table
 
 // Triplet defines model for Triplet.
-type Quadruplet struct {
+type Triplet struct {
 	Code *string `json:"code,omitempty"`
 	Name *string `json:"name,omitempty"`
-	Desc *string `json:desc,omitempty`
 	Slug *string `json:"slug,omitempty"`
 }
 
 // Categories defines model for Categories.
-type Categories []Quadruplet
+type Categories []Triplet
 
 type TopicCSV struct {
 	Code string
@@ -55,13 +61,13 @@ type TableCSV struct {
 type CategoryCSV struct {
 	Code  string
 	Name  string
-	Desc  string
 	Table string
 }
 
-const topicsFile = "census_atlas_metadata__-_topics.csv"
-const tableFile = "census_atlas_metadata__-_tables.csv"
-const catFile = "census_atlas_metadata__-_categories.csv"
+const topicsFile = "census_atlas_metadata__-_topics_(2011_metadata).csv"
+const tableFile = "census_atlas_metadata__-_tables_(2011_metadata).csv"
+const catFile = "census_atlas_metadata__-_categories_(2011_metadata).csv"
+const outputFile = "apiMetadata.json"
 
 func readCsvFile(filePath string) [][]string {
 	f, err := os.Open(filePath)
@@ -121,16 +127,92 @@ func parseCatsFromCSV() []CategoryCSV {
 			CategoryCSV{
 				Code:  t[0],
 				Name:  t[1],
-				Desc:  t[2],
-				Table: t[3],
+				Table: t[2],
 			},
 		)
 	}
 	return catsCSV
 }
 
+func isTotalCat(catName string) bool {
+	return strings.HasSuffix(catName, "0001")
+}
+
 func main() {
-	fmt.Println(parseTopicsFromCSV())
-	fmt.Println(parseTablesFromCSV())
-	fmt.Println(parseCatsFromCSV())
+	topicsRaw := parseTopicsFromCSV()
+	tablesRaw := parseTablesFromCSV()
+	catsRaw := parseCatsFromCSV()
+
+	// iterate through topics
+	var mdr MetadataResponse
+
+	for _, topic := range topicsRaw {
+		// get matching tables
+		var nestedTables Tables
+		for _, table := range tablesRaw {
+
+			if table.Topic == topic.Code {
+				// get matching cats
+				var nestedCats Categories
+				var total Triplet
+
+				for _, cat := range catsRaw {
+
+					if cat.Table == table.Code {
+						if isTotalCat(cat.Code) {
+							total = Triplet{
+								Code: spointer(cat.Code),
+								Name: spointer(cat.Name),
+								Slug: spointer(slug.Make(cat.Name)),
+							}
+						} else {
+							nestedCats = append(
+								nestedCats,
+								Triplet{
+									Code: spointer(cat.Code),
+									Name: spointer(cat.Name),
+									Slug: spointer(slug.Make(cat.Name)),
+								},
+							)
+						}
+					}
+				}
+				// append table
+				nestedTables = append(
+					nestedTables,
+					Table{
+						Categories: &nestedCats,
+						Code:       spointer(table.Code),
+						Name:       spointer(table.Name),
+						Desc:       spointer(table.Desc),
+						Slug:       spointer(slug.Make(table.Name)),
+						Total:      &total,
+					},
+				)
+			}
+
+		}
+		// append topic to mdr
+		mdr = append(
+			mdr,
+			Metadata{
+				Code:   spointer(topic.Code),
+				Name:   spointer(topic.Name),
+				Desc:   spointer(topic.Desc),
+				Slug:   spointer(slug.Make(topic.Name)),
+				Tables: &nestedTables,
+			},
+		)
+	}
+	var b []byte
+	var err error
+	b, err = json.Marshal(&mdr)
+	if err != nil {
+		panic(err)
+	}
+	_ = ioutil.WriteFile(outputFile, b, 0644)
+}
+
+func spointer(s string) *string {
+	return &s
 }
